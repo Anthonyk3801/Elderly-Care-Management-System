b21682ca9d6917b507b8e4fac0b8dd4c36bb2f76
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-11-23 08:55:24 -0500

    Create/Update Roles.sql file. Add all the roles and access level data.

---
+++ Roles.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `Roles`;
+
+CREATE TABLE `Roles` (
+  `role` varchar(50) NOT NULL,
+  `accessLevel` int(2) NOT NULL
+);
+
+INSERT INTO `Roles` (`role`, `accessLevel`)
+VALUES
+("Admin", 1),
+("Supervisor", 2),
+("Doctor", 3),
+("Caregiver", 4),
+("Patient", 5),
+("FamilyMember", 6);
