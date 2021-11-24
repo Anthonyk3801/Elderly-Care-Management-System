c57a57a1be69ae625c94aa434d5163cf0da5039d
Anthony Keller <anthonyk3801@gmail.com>
2021-11-24 09:06:42 -0500

    Added Search option and changed to update database

--- patientDoctor.php
+++ patientDoctor.php
@@ -5,7 +5,13 @@
 <head>
   <link rel="stylesheet" href="CSS/patientDoctorStyle.css">
 </head>
+<form action="patientDoctor.php" method="POST">
+    <label for="search">Date Search:</label>
+    <input type="text" name="search" id="search" required>
+    <input type="submit" id="enter" value="Submit">
+</form>
 <button onclick="test()" id="newPerscription">New Perscription</button>
+<form action="patientDoctor.php" method="POST">
 <div id="hide">
 <table>
   <tr>
@@ -15,13 +21,12 @@
   <th>Night Med</th>
   </tr>
   <tr>
-    <td><input type="text"></td>
-    <td><input type="text"></td>
-    <td><input type="text"></td>
-    <td><input type="text"></td>
+    <td><input type="text" name="comment" id="comment"></td>
+    <td><input type="text" name="morningMed" id="morningMed"></td>
+    <td><input type="text" name="lunchMed" id="lunchMed"></td>
+    <td><input type="text" name="nightMed" id="nightMed"></td>
   </tr>
 </table>
-<form action="patientDoctor.php">
   <input type="submit" value="submit" id="submit">
 </form>
 </div>
@@ -35,14 +40,20 @@ function test() {
 }
 </script>
 <?php
+$search = $_POST['search'];
+$sql = "SELECT *
+FROM DoctorAppointments
+WHERE date like '%$search%'";
+$result = $conn->query($sql);
+
 if(isset($_POST['submit'])){
     $comment = $_POST['comment'];
     $morningMed = $_POST['morningMed'];
     $afternoonMed = $_POST['lunchMed'];
     $nightMed = $_POST['nightMed'];
 };
-if (isset($_POST['submit'])){
-  $sql = "INSERT INTO DoctorAppointments (patientID, date, time, comment, morningMed, lunchMed, nightMed)
-  VALUES ('patientID', 'date', 'time', '$comment', '$morningMed', '$lunchMed', '$nightMed')";
+if(isset($_POST['submit'])){
+  $sql = "UPDATE DoctorAppointments
+  SET comment='comment', morningMed='morningMed', lunchMed='lunchMed', nightMed='nightMed')";
 }
 ?>
