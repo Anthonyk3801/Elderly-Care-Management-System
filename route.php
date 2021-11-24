937619711ffb4b213aeff65694e232eca3ee3899
Jody Winters <jodywinters11@hotmail.com>
2021-11-24 08:30:27 -0500

    Login Page works and Session variables are stored

---
+++ route.php
@@ -0,0 +1,100 @@
+<?php
+    
+include 'db_connection.php';
+
+if(isset($_POST['login'])){
+    $email = strtolower($_POST['email']);
+    $check = false;
+    $table = '';
+    $sql="Select * FROM Patient 
+    WHERE LOWER(email) = '$email' AND password = '$_POST[password]';";
+
+    $result = $conn->query($sql);
+
+    $cnt = mysqli_num_rows($result);
+    if ( 0===$cnt ) {
+        //echo 'no patient records <br>';
+    }else {
+        while( false!=($res=mysqli_fetch_array($result)) ) {
+            $check = true;
+            $table = 'Patient';
+        }
+
+    }
+    $sql="Select * FROM FamilyMember 
+    WHERE LOWER(email) = '$email' AND password = '$_POST[password]';";
+
+    $result = $conn->query($sql);
+
+    $cnt = mysqli_num_rows($result);
+    if ( 0===$cnt ) {
+        //echo 'no family member records <br>';
+    }else {
+        while( false!=($res=mysqli_fetch_array($result)) ) {
+            $check = true;
+            $table = 'FamilyMember';
+        }
+
+    }
+    $sql="Select * FROM Employee
+    WHERE LOWER(email) = '$email' AND password = '$_POST[password]';";
+
+    $result = $conn->query($sql);
+
+    $cnt = mysqli_num_rows($result);
+    if ( 0===$cnt ) {
+        //echo 'no employee records <br>';
+    }else {
+        while( false!=($res=mysqli_fetch_array($result)) ) {
+            $check = true;
+            $table = 'Employee';
+        }
+    }
+    if($check === false){
+        header('location:login.php?error=1');
+    }elseif($check === true){
+        session_start();
+        if($table == 'FamilyMember') {
+            $_SESSION['level'] = 6;
+            $_SESSION['role'] = 'FamilyMember';
+        }elseif($table == 'Employee'){
+            $sql = "Select role,employeeID from Employee where LOWER(email) = '$email';";
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
+            $_SESSION['role'] = $row['role'];
+            $_SESSION['id'] = $row['employeeID'];
+
+            $role = $_SESSION['role'];
+            $sql = "Select accessLevel from Roles where role = '$role';";
+            $result = $conn->query($sql);
+            $res = $result->fetch_assoc();
+            $_SESSION['level'] = $res['accessLevel'];
+            
+        }elseif($table == 'Patient') {
+            $sql = "Select patientID from Patient where LOWER(email) = '$email';";
+            $result = $conn->query($sql);
+            $res = $result->fetch_assoc();
+            $_SESSION['id'] = $res['patientID'];
+            $_SESSION['level'] = 5;
+            $_SESSION['role'] = 'Patient';
+        }
+
+        $sql = "Select * from $table WHERE LOWER(email) = '$email';";
+
+        $result = $conn->query($sql);
+        $res = $result->fetch_assoc();
+        $_SESSION['fName'] = $res['fName'];
+        $_SESSION['lName'] = $res['lName'];
+
+        /*
+        echo "First: " . $_SESSION['fName'] . "<br>";
+        echo "Last: " . $_SESSION['lName'] . "<br>";
+        echo "Access Level: " . $_SESSION['level'] . "<br>";
+        echo "Role: " . $_SESSION['role'] . "<br>";
+        if($table == 'Patient' || $table == 'Employee') echo "ID: " . $_SESSION['id'];
+        */
+    }   
+
+}
+
+?>
