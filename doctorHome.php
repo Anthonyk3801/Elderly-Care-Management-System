6d42870369c7fd211db87abe42f7917f9c424307
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-06 11:26:18 -0500

    Add template to doctorHome.php.

--- doctorHome.php
+++ doctorHome.php
@@ -6,5 +6,26 @@ if(isset($_SESSION['level'])){
 };
 if($_GET['error'] == 2) echo "You are not authorized for that page";
 */
-    echo "Doctor Home";
+?>
+
+<?php //TEMPLATES
+    include 'templates/header.html';
+    //include 'templates/alert-message-before-login.html';
+    include 'templates/nav-bar.html';
+    include 'templates/main-grid-content-1column.html';
+    //include 'templates/main-grid-content-2columns.html';
+    //include 'templates/side-bar.html';
+    //include 'templates/side-bar-hidden.html';
+    include 'templates/main-content.html';
+    //include 'templates/end-main-content.html';
+    //include 'templates/footer.html';
+?>
+
+<h1>Doctor's Home</h1>
+<hr>
+<br>
+
+<?php // TEMPLATES
+  include 'templates/end-main-content.html';
+  include 'templates/footer.html';
 ?>
