939e48d23edbd1f460f09bbd273fb7ebb6c9b767
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-06 11:24:50 -0500

    Add template to newRoster.php

--- newRoster.php
+++ newRoster.php
@@ -5,5 +5,26 @@ if(isset($_SESSION['level'])){
     if($_SESSION['level'] != 1 || $_SESSION['level'] != 2) header('location:extras/transfer.php');
 };
 */
-echo "new roster page";
+?>
+
+<?php //TEMPLATES
+    include 'templates/header.html';
+    //include 'templates/alert-message-before-login.html';
+    include 'templates/nav-bar.html';
+    include 'templates/main-grid-content-1column.html';
+    //include 'templates/main-grid-content-2columns.html';
+    //include 'templates/side-bar.html';
+    //include 'templates/side-bar-hidden.html';
+    include 'templates/main-content.html';
+    //include 'templates/end-main-content.html';
+    //include 'templates/footer.html';
+?>
+
+<h1>New Roster</h1>
+<hr>
+<br>
+
+<?php // TEMPLATES
+  include 'templates/end-main-content.html';
+  include 'templates/footer.html';
 ?>
