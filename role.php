89c7ed931e443ff671be1c83e9fb3065a25ae502
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-06 10:29:41 -0500

    Add template to role.php.

--- role.php
+++ role.php
@@ -1,17 +1,31 @@
 <?php
 session_start();
-if(!isset($_SESSION['level'])) header('location:home.php');
-if(isset($_SESSION['level'])){
-    if($_SESSION['level'] != 1) header('location:extras/transfer.php');
-};
+//if(!isset($_SESSION['level'])) header('location:home.php');
+//if(isset($_SESSION['level'])){
+//    if($_SESSION['level'] != 1) header('location:extras/transfer.php');
+//};
 include 'db_connection.php';
 ?>
-<html>
-    <head>
-    <link rel="stylesheet" href="CSS/roleStyles.css">
-    </head>
-    <body>
-        <h1>Enter a new role into the database</h1>
+
+
+<?php //TEMPLATES
+    include 'templates/header.html';
+    //include 'templates/alert-message-before-login.html';
+    include 'templates/nav-bar.html';
+    include 'templates/main-grid-content-1column.html';
+    //include 'templates/main-grid-content-2columns.html';
+    //include 'templates/side-bar.html';
+    //include 'templates/side-bar-hidden.html';
+    include 'templates/main-content.html';
+    //include 'templates/end-main-content.html';
+    //include 'templates/footer.html';
+?>
+
+        <h1>Role</h1>
+        <hr>
+        <br>
+        <h2>Enter a new role into the database</h2>
+        <br>
         <form action="role.php" method="POST">
             <label for="role">New Role</label>
             <input type="text" name="role" id="role" required>
@@ -21,8 +35,14 @@ include 'db_connection.php';
 
             <input type="submit" value="submit">
         </form>
-    </body>
-</html>
+        <br>
+
+<?php // TEMPLATES
+include 'templates/end-main-content.html';
+include 'templates/footer.html';
+?>
+
+
 <?php
 if(isset($_POST['role'])){
     $sql = "SELECT * FROM Roles WHERE role = '$_POST[role]'";
