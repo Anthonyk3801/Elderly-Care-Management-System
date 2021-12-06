93ce1a900acb6521cb7cd78876ff8d34f6311fa4
Jody Winters <jodywinters11@hotmail.com>
2021-12-06 10:58:36 -0500

    Finished backend of adminReport

--- adminReport.php
+++ adminReport.php
@@ -13,6 +13,10 @@ if(isset($_POST['search'])) {
     where date='$_POST[date]'";
     $result = $conn->query($sql);
 }
+$doctorName = 0;
+$attendance = 0;
+$time = 0;
+$caregiverName = 0;
 
 
 ?>
@@ -30,9 +34,10 @@ if(isset($_POST['search'])) {
 
         <table>
             <th>Patient Name</th>
+            <th>Patient ID</th>
             <th>Doctor Name</th>
             <th>Caregiver Name</th>
-            <th>Doctor Appointment</th>
+            <th>Appointment Time</th>
             <th>Attendend Appointment</th>
             <th>Breakfast</th>
             <th>Morning Med</th>
@@ -45,23 +50,90 @@ if(isset($_POST['search'])) {
             <?php 
             
             while($res = mysqli_fetch_array($result)) { 
+                //Getting all of the doctor information if there is an appointment
                 $sql="SELECT * FROM DoctorAppointments WHERE date='$_POST[date]' AND patientID=$res[patientID]";
                 $result2 = $conn->query($sql);
                 $appoint = $result2->fetch_assoc();
+                $cnt = mysqli_num_rows($result2);
+                if ( 0===$cnt ) {
+                    $doctorName = "No App";
+                    $attendance = "No App";
+                    $time = "No App";
+                }else {
+                    $sql="SELECT * FROM Employee WHERE employeeID = $appoint[doctorID]";
+                    $result3 = $conn->query($sql);
+                    $doc = $result3->fetch_assoc();
+                    $doctorName = $doc['fName'] . " " . $doc['lName'];
+                    $attendance = $appoint['attendance'];
+                    $time = $appoint['time'];
+                }
+                if($res['breakfast'] == 0 || $res['lunch'] == 0 || $res['dinner'] == 0 || $res['morningMedCheck'] == 0 ||
+                $res['lunchMedCheck'] == 0 || $res['nightMedCheck'] == 0 || $attendance == 0){
+                //Getting the patients name from their ID
+                $sql = "SELECT * FROM Patient WHERE patientID = $res[patientID] AND approval = 1";
+                $result4 = $conn->query($sql);
+                $patient = $result4->fetch_assoc();
+                $group = $patient['groupID'];
+
+                //Getting name of the caregiver
+                $sql = "SELECT * FROM Roster WHERE date='$_POST[date]'";
+                $result5 = $conn->query($sql);
+                $care = $result5->fetch_assoc();
+                if($group == 1) $caregiverName = $care['careGiver1'];
+                if($group == 2) $caregiverName = $care['careGiver2'];
+                if($group == 3) $caregiverName = $care['careGiver3'];
+                if($group == 4) $caregiverName = $care['careGiver4'];
+
+                $sql="SELECT * FROM Employee WHERE employeeID = $caregiverName";
+                $result6 = $conn->query($sql);
+                $care = $result6->fetch_assoc();
+
+
                 
                 echo "<tr>";
+                echo "<td>".$patient['fName'] . " " . $patient['lName'] . "</td>";
                 echo "<td>".$res['patientID']."</td>";
-                echo "<td>".$appoint['doctorID']."</td>";
-                echo "<td> caregiver name </td>";    
-                echo "<td>".$appoint['time']."</td>";
-                echo "<td>".$appoint['attendance']."</td>";
-                echo "<td>".$res['breakfast']."</td>";
-                echo "<td>".$res['morningMedCheck']."</td>";
-                echo "<td>".$res['lunch']."</td>";
-                echo "<td>".$res['lunchMedCheck']."</td>";
-                echo "<td>".$res['dinner']."</td>";
-                echo "<td>".$res['nightMedCheck']."</td>";
-                        
+                echo "<td>".$doctorName."</td>";
+                echo "<td>".$care['fName'] . " " . $care['lName'] . "</td>";    
+                echo "<td>".$time."</td>";
+                if($attendance == 0) {
+                    echo "<td> X </td>";
+                }elseif($attendance == 1){
+                    echo "<td> &#10004; </td>";
+                }else{
+                    echo "<td> No App </td>";
+                }
+                if($res['breakfast'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                if($res['morningMedCheck'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                if($res['lunch'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                if($res['lunchMedCheck'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                if($res['dinner'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                if($res['nightMedCheck'] == 0) {
+                    echo "<td> X </td>";
+                }else{
+                    echo "<td> &#10004; </td>";
+                }
+                }       
             }
             ?> 
 
