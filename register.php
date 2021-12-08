a493d3dca1350d246e0c15d2eae246a1780b26ac
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-08 10:58:37 -0500

    Updated the register.php and login.php pages, better css, better forms.

--- register.php
+++ register.php
@@ -26,89 +26,79 @@
     //include 'templates/footer.html';
 ?>
 
-<h1>Sign Up</h1>
-<hr>
-<br>
-<form action="register.php" method="POST">
-        <label for="role">Choose a role:</label>
-        <select id="role" name="role">
-            <option value="nothing"> ---</option>
-            <?php
-            while($res = mysqli_fetch_array($result)) {
-            ?>
-                 <option value="<?php echo $res['role'];?>"><?php echo $res['role'];?></option>;
-            <?php
-            }
-            ?>
-        </select>
+<main class="form-signing">
+  <form action="register.php" method="POST">
+    <h1 class="h1 mb-3 fw-normal text-center">Sign Up</h1>
+    <hr>
+    <label for="role">Choose a role:</label>
+    <select id="role" name="role">
+        <option value="nothing"> --- </option>
+        <?php
+        while($res = mysqli_fetch_array($result)) {
+        ?>
+             <option value="<?php echo $res['role'];?>"><?php echo $res['role'];?></option>;
+        <?php
+        }
+        ?>
+    </select>
+    <input type="submit" value="Submit">
+  </form>
 
-        <input type="submit" value="Submit">
-
-</form>
-
-​
-<form action="home.php" method="POST">
-
-    <label for="role2">Role</label>
-    <input readonly type="text" id="role2" name="role2" value="<?php echo $_POST['role'] ?>">
-
-    <br>
-
+  <form action="home.php" method="POST">
+  <div class="form-floating mb-3 mt-3">
+    <input type="text" id="fName" name="fName" class="form-control" placeholder="First Name">
     <label for="fName">First Name</label>
-    <input type="text" id="fName" name="fName">
-​
-    <br>
-​
-    <label for="lName">Last Name:</label>
-    <input type="text" id="lName" name="lName">
-​
-    <br>
-​
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email">
+  </div>
 
-    <br>
-​
-    <label for="phone">Phone #:</label>
-    <input type="tel" id="phone" name="phone">
-​
-    <br>
-​
-    <label for="password">Password:</label>
-    <input type="text" id="password" name="password">
-​
-    <br>
-​
-    <label for="DOB">D.O.B:</label>
-    <input type="date" id="DOB" name="DOB">
+  <div class="form-floating mb-3 mt-3">
+    <input type="text" id="lName" name="lName" class="form-control" placeholder="Last Name">
+    <label for="lName">Last Name</label>
+  </div>
 
-    <br>
-​
-    <div id='patientOnly'>
-        <label for="familyCode">Family Code:</label>
-        <input type="number" id="familyCode" name="familyCode" min='100' max='999' <?php if($_POST['role'] != 'Patient') echo 'disabled' ?>>
-​
-        <br>
-​
-        <label for="emergencyContactName">Emergency Contact Name:</label>
-        <input type="text" id="emergencyContactName" name="emergencyContactName" <?php if($_POST['role'] != 'Patient') echo 'disabled' ?>>
+  <div class="form-floating mb-3 mt-3">
+    <input type="email" id="email" name="email" class="form-control" placeholder="name@example.com">
+    <label for="email">Email address</label>
+  </div>
 
-        <br>
-​
-        <label for="emergencyContactNum">Emergency Contact #:</label>
-        <input type="tel" id="emergencyContactNum" name="emergencyContactNum" <?php if($_POST['role'] != 'Patient') echo 'disabled' ?>>
-​
-        <br>
-​
-        <label for="emergencyContactRelation">Emergency Contact Relation:</label>
-        <input type="text" id="emergencyContactRelation" name="emergencyContactRelation" <?php if($_POST['role'] != 'Patient') echo 'disabled' ?>>
-    </div>
+  <div class="form-floating mb-3 mt-3">
+    <input type="tel" id="phone" name="phone" class="form-control" placeholder="000-000-0000">
+    <label for="phone">Phone #</label>
+  </div>
 
-    <br>
-​
-    <input type="submit" id="sub" value="Submit" name='sub'>
-​
+  <div class="form-floating mb-3 mt-3">
+    <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+    <label for="password">Password</label>
+  </div>
+
+  <div class="form-floating mb-3 mt-3">
+    <input type="date" id="DOB" name="DOB" class="form-control" placeholder="Date of Birth">
+    <label for="DOB">Date of Birth</label>
+  </div>
+
+<div class="form-floating mb-3 mt-3">
+  <input type="number" id="familyCode" name="familyCode" min='100' max='999' class="form-control" placeholder="Family Code:" <?php if($_POST['role'] != 'Patient') echo 'hidden' ?>>
+  <label for="familyCode">Family Code:</label>
+</div>
+
+<div class="form-floating mb-3 mt-3">
+  <input type="text" id="emergencyContactName" name="emergencyContactName" class="form-control" placeholder="Emergency Contact Name:" <?php if($_POST['role'] != 'Patient') echo 'hidden' ?>>
+  <label for="emergencyContactName">Emergency Contact Name:</label>
+</div>
+
+<div class="form-floating mb-3 mt-3">
+  <input type="tel" id="emergencyContactNum" name="emergencyContactNum" class="form-control" placeholder="Emergency Contact #:" <?php if($_POST['role'] != 'Patient') echo 'hidden' ?>>
+  <label for="emergencyContactNum">Emergency Contact #:</label>
+</div>
+
+<div class="form-floating mb-3 mt-3">
+  <input type="text" id="emergencyContactRelation" name="emergencyContactRelation" class="form-control" placeholder="Emergency Contact Relation:" <?php if($_POST['role'] != 'Patient') echo 'hidden' ?>>
+  <label for="emergencyContactRelation">Emergency Contact Relation:</label>
+</div>
+
+<button class="w-100 btn btn-lg btn-primary" type="submit" id="sub" value="Submit" name='sub'>Register</button>
+
 </form>
+</main>
 
 <?php // TEMPLATES
   include 'templates/end-main-content.html';
