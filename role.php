6b63137ecbea15e3c8b24c50f6af647d3e7dfd77
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-10 09:04:08 -0500

    Add divs & classes, add cancel button, improve styles..

--- role.php
+++ role.php
@@ -21,21 +21,32 @@ include 'db_connection.php';
     //include 'templates/footer.html';
 ?>
 
-        <h1>Role</h1>
+        <h1 class="text-center">Role</h1>
         <hr>
+
+        <div class="mb-5 mt-5 text-center">
+
+        <h2>Enter a new role into the database:</h2>
         <br>
-        <h2>Enter a new role into the database</h2>
-        <br>
+
         <form action="role.php" method="POST">
+
             <label for="role">New Role</label>
+            <br>
             <input type="text" name="role" id="role" required>
-
+            <br>
+            <br>
             <label for="accessLevel">Access Level</label>
+            <br>
             <input type="text" name="level" id="accessLevel" maxlength="1" placeholder="1-6" required>
+            <br>
 
-            <input type="submit" value="submit">
+            <button class="w-75 btn btn-sm btn-info text-light mt-5 mb-1" type="submit" value="Submit">Submit</button>
+            <button class="w-75 btn btn-sm btn-secondary text-light mt-1 mb-1" type="reset">Cancel</button>
+
         </form>
-        <br>
+
+      </div>
 
 <?php // TEMPLATES
 include 'templates/end-main-content.html';
