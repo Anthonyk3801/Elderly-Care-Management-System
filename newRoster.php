8df15bcc03eb84cb808c56329b2be3940bbc55af
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-13 16:29:10 -0500

    Add divs/classes, add buttons, improve css, etc..

--- newRoster.php
+++ newRoster.php
@@ -25,11 +25,11 @@ if(isset($_POST['create'])){
   $arr = array($_POST['supervisor'],$_POST['doctor'],$_POST['caregiver1'],$_POST['caregiver2'],$_POST['caregiver3'],$_POST['caregiver4']);
   $arr = array_unique($arr);
   if(count($arr) < 6) $check = 2;
-  
+
   foreach($arr as $person){
    if($person == 'nothing') $check = 0;
   }
-  
+
 
   if($check == 1){
     $sql = "DELETE FROM Roster WHERE date='$_POST[date]'";
@@ -61,10 +61,14 @@ if(isset($_POST['create'])){
     //include 'templates/end-main-content.html';
     //include 'templates/footer.html';
 ?>
+<!-- <link rel="stylesheet" href="CSS/newRoster.css"> -->
+<head>
+  <link rel="stylesheet" href="CSS/newRoster.css">
+</head>
 
-<h1>New Roster</h1>
+<h1 class="text-center">New Roster</h1>
 <hr>
-<br>
+
 <?php
 if(isset($_POST['ok']) || isset($_POST['create']) && $created == 0){
   $sql = "SELECT * FROM Roster WHERE date = '$_POST[date]'";
@@ -72,27 +76,26 @@ if(isset($_POST['ok']) || isset($_POST['create']) && $created == 0){
 
   $cnt = mysqli_num_rows($result);
   if ( 0===$cnt ) {
-      
+
   }else {
       echo "<p style='color:red;'>Warning: There is already a roster for that day. Creating a roster will update the previous one.</p>";
-    
+
   }
 }
 if($check == 0) echo "All Fields are Required";
 if($check == 2) echo "All Fields Must Be Different";
 if($created == 1) echo "New Roster was Created!";
-
 ?>
-<head>
-  <link rel="stylesheet" href="CSS/newRoster.css">
-</head>
+
+<div class="mt-5 mb-5 text-dark text-center">
 <form action="newRoster.php" method="post">
   <label for="date">Date: </label>
-  <input type="date" name="date" id="date" value=<?php if(isset($_POST['ok']) || isset($_POST['create']))echo $_POST['date'];?>>
-  <input type="submit" name="ok" id="ok" value="OK">
+  <input class="text-end" type="date" name="date" id="date" value=<?php if(isset($_POST['ok']) || isset($_POST['create']))echo $_POST['date'];?>>
+  <button class="btn btn-sm btn-info text-light mt-1 mb-1" type="submit" name="ok" id="ok" value="OK">OK</button>
+
   <div class="formcss">
     <label for="supervisor">Supervisor: </label>
-    <select name="supervisor" id="supervisor">
+    <select class="text-center" name="supervisor" id="supervisor">
         <option value="nothing">---</option>
         <?php
         while($res = mysqli_fetch_array($sresult)) {
@@ -104,7 +107,7 @@ if($created == 1) echo "New Roster was Created!";
     </select>
 
     <label for="doctor">Doctor: </label>
-    <select name="doctor" id="doctor">
+    <select class="text-center" name="doctor" id="doctor">
         <option value="nothing">---</option>
         <?php
         while($res = mysqli_fetch_array($dresult)) {
@@ -116,7 +119,7 @@ if($created == 1) echo "New Roster was Created!";
     </select>
 
     <label for="caregiver1">Caregiver 1: </label>
-    <select name="caregiver1" id="caregiver1">
+    <select class="text-center" name="caregiver1" id="caregiver1">
         <option value="nothing">---</option>
         <?php
         //Getting all caregivers
@@ -133,7 +136,7 @@ if($created == 1) echo "New Roster was Created!";
 
   <div class="formcss">
     <label for="caregiver2">Caregiver 2: </label>
-    <select name="caregiver2" id="caregiver2">
+    <select class="text-center" name="caregiver2" id="caregiver2">
         <option value="nothing">---</option>
         <?php
         //Getting all caregivers
@@ -148,7 +151,7 @@ if($created == 1) echo "New Roster was Created!";
     </select>
 
     <label for="caregiver3">Caregiver 3: </label>
-    <select name="caregiver3" id="caregiver3">
+    <select class="text-center" name="caregiver3" id="caregiver3">
         <option value="nothing">---</option>
         <?php
         //Getting all caregivers
@@ -163,7 +166,7 @@ if($created == 1) echo "New Roster was Created!";
     </select>
 
     <label for="caregiver4">Caregiver 4: </label>
-    <select name="caregiver4" id="caregiver4">
+    <select class="text-center" name="caregiver4" id="caregiver4">
         <option value="nothing">---</option>
         <?php
         //Getting all caregivers
@@ -178,14 +181,11 @@ if($created == 1) echo "New Roster was Created!";
     </select>
   </div>
 
-  <input class="submity" type="submit" name="create" id="create" value="CREATE">
-
-
-
-
+  <button class="w-100 btn btn-sm btn-info text-light mt-4" type="submit" name="create" id="create" value="CREATE">CREATE</button>
+  <button class="w-100 btn btn-sm btn-secondary text-light mt-1" type="reset">Cancel</button>
 
 </form>
-
+</div>
 
 <?php // TEMPLATES
   include 'templates/end-main-content.html';
