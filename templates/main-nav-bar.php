70048ce16c8bda1441d3117aa50746a6f34c2c52
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-12-15 09:03:06 -0500

    Updated website templates for the side bar, and main nav, etc.

---
+++ templates/main-nav-bar.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="text-end">
+    <button type="button" class="btn bg-info me-2 text-light"><?php echo $_SESSION['fName']. " " . $_SESSION['lName'];?></button>
+    <button type="button" class="btn btn-danger text-light" value="logout" onclick="window.location.href='logout.php';">Log Out</button>
+  </div>
+</div>
+
+</header>
+<!-- END OF NAV BAR -->
